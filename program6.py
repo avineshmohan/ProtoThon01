4077d50ed77da09a476fdf404694910b854e9d46
avineshmohan <avineshmohan@outlook.com>
2019-03-16 11:59:04 +0530

    program 6

---
+++ program6.py
@@ -0,0 +1,6 @@
+value = input('Enter the String: ')
+
+print("All caps: ",value.upper())
+print('Small caps: ',value.lower())
+print('Title Case: ',value.title())
+print('Start Case: ',value.capitalize())
