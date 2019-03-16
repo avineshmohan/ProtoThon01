400ae835455efa90c78903a6d3ed2c4c179bcc38
avineshmohan <avineshmohan@outlook.com>
2019-03-16 13:22:37 +0530

    RMS Value

---
+++ Program2.py
@@ -0,0 +1,18 @@
+import math 
+
+value = input('Enter the values')
+
+mylist = value.split(",")
+n = len(mylist)
+for i in range (n):
+    mylist[i] = int(mylist[i])
+for i in range (n):
+    mylist[i] *= mylist[i]
+
+    
+    mean = sum(mylist)/float(n)
+    
+    
+    root = math.sqrt(mean)
+    
+print(root)
